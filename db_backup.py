601fe1a1225f83852e480baf1131a7581ee399a5
Andres Vara <andres.vara@matchesfashion.com>
2016-03-22 16:06:21 +0000

    cleaned from debuging

--- db_backup.py
+++ db_backup.py
@@ -6,7 +6,6 @@ import string
 import getopt
 import logging
 
-
 from datetime import datetime
 
 def menu():
@@ -21,28 +20,30 @@ def menu():
   message = string.join(menu)
   print message
 
-def backup(keep=7, databases=None, store=None, user=None, password=None, host=None):
+# def delete_old_backups(keep):
+
+
+def backup(databases=None, store=None, user=None, password=None, host=None):
   # get current date
   timestamp = datetime.now().strftime("%Y-%m-%d")
-  
+
   if databases != None:
     for db in databases:
+      db = db.strip() # remove leading and trailing whitespace
       db_backup_name = string.join([db, timestamp, 'sql'], '.')
       db_backup_path = store + os.sep + db_backup_name
 
       dump_cmd = 'mysqldump -u ' + user
       if host != None:
-        dump_cmd += ' -h ' + "'" + host + "'" 
+        dump_cmd += ' -h ' + "'" + host + "'"
       if password != None:
         dump_cmd += ' -p' + password
       dump_cmd += ' -e --opt -c ' + db + ' | gzip > ' + db_backup_path + '.gz'
+      logging.info("Dump db, %s to %s." % (db, db_backup_path))
+      os.popen(dump_cmd)
       print dump_cmd
 
-
-
-
 def main(argv):
-
   # Set default vals
   keep      = 7
   databases = None
@@ -52,9 +53,6 @@ def main(argv):
   store     = None
 
   opts, args = getopt.getopt(argv, "hn:k:d:t:u:p:s:", ["help", "keep=", "databases=", "store=", "user=", "password=", "host="])
-
-  # print opts
-  # print args
 
   if len(argv) == 0:
     menu()
@@ -77,9 +75,7 @@ def main(argv):
     elif opt in ('-s', '--host'):
       host = arg
 
-  # print databases
-  # print type(databases)
-  backup(keep, databases, store, user, password, host)
+  backup(databases, store, user, password, host)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
